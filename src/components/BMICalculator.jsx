f4080ffe826814e142902e021b41360092235e96
Purushottam Sonar <sonarp02@gmail.com>
2023-11-27 00:50:55 +0530

    all html are added

--- src/components/BMICalculator.jsx
+++ src/components/BMICalculator.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const BMICalculator = () => {
-  return <></>;
+  return (
+    <>
+      <div>
+        <div>
+          <h1>BMI Calculator</h1>
+        </div>
+        <div>
+          <input type="number" placeholder="Enter Weight (kg)" />
+        </div>
+        <div>
+          <input type="number" placeholder="Enter Height (m)" />
+        </div>
+        <div>
+          <button type="button">Calculate BMI</button>
+        </div>
+        <div>
+          <h1>Your BMI : </h1>
+          <h2>Category : </h2>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BMICalculator;
